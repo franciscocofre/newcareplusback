ad7c17f3e64851ca9a1fb72bfb5585eacbb7673e
francisco <franciscocofremaueln@gmail.com>
2024-12-06 16:58:35 -0300

    Actualizando importaciones con extenciones 9.2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const sequelize = require('./config/config');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 
 // Configurar CORS
